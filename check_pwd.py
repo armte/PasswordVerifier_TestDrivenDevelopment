63cc2a5e0d3e26cd72b2f57a63e95eb6c3958fa9
Tommy Armstrong <tearmstrong312@gmail.com>
2020-05-09 13:48:40 -0700

    Made check_pwd return False if a password does not contain at least 1 uppercase letter, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -8,6 +8,8 @@ import re
 def check_pwd(pwd):
     if len(pwd) < 8 or 20 < len(pwd):
         return False
-    if re.search('[a-z]', pwd) == None:
+    if re.search('[a-z]', pwd) is None:
+        return False
+    if re.search('[A-Z]', pwd) is None:
         return False
     return True
