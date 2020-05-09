eaae71c85261193a4d9bf2d3c25c61c06dfdfc09
Tommy Armstrong <tearmstrong312@gmail.com>
2020-05-09 13:07:26 -0700

    Wrote test1 to see if an empty string is rejected correctly

---
+++ check_pwd.py
@@ -0,0 +1,8 @@
+# Author: Tommy Armstrong
+# Date: 5/9/2020
+# Class: CS 362 - Software Engineering II
+# Assignment: A2 - TDD Hands On
+
+
+def check_pwd(pwd):
+    pass
---
+++ tests.py
@@ -0,0 +1,24 @@
+# Author: Tommy Armstrong
+# Date: 5/9/2020
+# Class: CS 362 - Software Engineering II
+# Assignment: A2 - TDD Hands On
+
+import unittest
+import random
+import string
+from check_pwd import check_pwd
+
+def create_pwd(length):
+    return random.choices((string.ascii_leters + string.digits), length)
+
+
+class TestPwdChecker(unittest.TestCase):
+
+    def test1(self):
+        pwd = ''
+        expected = False
+        self.assertEqual(check_pwd(pwd), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
